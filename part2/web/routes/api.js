167f3abff7d77c0b0a6252133bcc91b3a2deff7d
yeyoung8805 <yyk8805@naver.com>
2022-03-30 15:57:40 +0900

    express refactoring

--- part2/web/routes/api.js
+++ part2/web/routes/api.js
@@ -2,7 +2,13 @@ var express = require('express'); //import express from 'express' 와 같은 의
 var router = express.Router();
 
 router.get('/first', function(req, res) {
-  res.json({id: 1, name: 'first'}); //json 대신에 send 들어가면 텍스트가 나온다. render 라고 하면 템플릿이 나온다.
+  const obj = {
+    name: 'John',
+    age: 30,
+    job: 'developer',
+    city: 'New York'
+  };
+  res.json(obj); //res.json 대신에 res.send 들어가면 텍스트가 나온다. res.render 라고 하면 템플릿이 나온다.
 });
 
 module.exports = router;
