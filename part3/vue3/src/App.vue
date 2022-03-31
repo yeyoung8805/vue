57453af49c60aa16664ee85dee900ef3d1d6483d
yeyoung8805 <yyk8805@naver.com>
2022-03-31 10:50:38 +0900

    add footer and logo image

--- part3/vue3/src/App.vue
+++ part3/vue3/src/App.vue
@@ -2,11 +2,16 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import TheFooter from './components/TheFooter.vue'
+
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div>
+    <img alt="Vue logo" src="https://nam.edu/wp-content/uploads/2021/02/blue-sky-300x169.png" />
+    <HelloWorld msg="Hello Vue 3 + Vite" />
+    <TheFooter msg="by yeyoung" />
+  </div>
 </template>
 
 <style>
