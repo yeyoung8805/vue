896b52971387e6f14c9652bb4befdace614925d5
yeyoung8805 <yyk8805@naver.com>
2022-03-31 16:18:20 +0900

    get example

---
+++ part3/example/src/components/GetExample.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <ul>
+      <li>
+        <button @click="get">get</button>
+      </li>
+    </ul>
+    <code id="result"></code>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  setup() {
+    const headers = {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    };
+
+    function showResult(json) {
+      document.getElementById("result").innerHTML = JSON.stringify(json);
+    }
+
+    function get() {
+      console.log("get");
+      fetch("https://jsonplaceholder.typicode.com/posts/1/comments", {
+        method: "GET",
+        headers,
+      })
+        .then(function (response) {
+          return response.json();
+        })
+        .then((json) => {
+          showResult(json);
+        });
+    }
+
+    return { get };
+  },
+};
+</script>
